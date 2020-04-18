089ddab04c57ddc372e5ae0d9b09980441c473ac
Thierry Deo <thierry.deo@gmail.com>
2020-04-18 19:10:42 +0200

    Show player list

--- src/Perudo/Game.js
+++ src/Perudo/Game.js
@@ -10,11 +10,17 @@ import Me from './Me';
 
 const Game = ({ players, events, previousBet, me }) => {
   return <Row>
-    <Col xs={12} sm={6} md={4}>
+    <Col xs={12} sm={6} md={4} className="mb-3">
       <Events events={events} />
     </Col>
 
     <Col xs={12} sm={6} md={8}>
+      <Row className="mb-3">
+        <Col xs={12}>
+          Joueurs en lice:
+          {players.map(p => `${p.name} (${p.diceCount} dés)`).join(', ')}
+        </Col>
+      </Row>
       <Me {...me} />
     </Col>
   </Row>;
